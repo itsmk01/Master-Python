67be1965f1da3178b5c06046892112d7ebee0e9f
ITSMK01 <manishkumar77937@gmail.com>
2024-10-05 22:47:15 +0530

    Coffee Machine Completed

--- coffee_machine.py
+++ coffee_machine.py
@@ -1,10 +1,24 @@
+import os
+
+def clear():
+    # For Windows
+    if os.name == 'nt':
+        os.system('cls')
+    # For Mac and Linux (name is 'posix')
+    else:
+        os.system('clear')
+
+# Example usage
+
+
 MENU = {
     "espresso": {
         "ingredients": {
             "water": 50,
+            "milk" : 0,
             "coffee": 18,
         },
-        "cost": 1.5,
+        "cost": 50,
     },
     "latte": {
         "ingredients": {
@@ -12,7 +26,7 @@ MENU = {
             "milk": 150,
             "coffee": 24,
         },
-        "cost": 2.5,
+        "cost": 150,
     },
     "cappuccino": {
         "ingredients": {
@@ -20,15 +34,16 @@ MENU = {
             "milk": 100,
             "coffee": 24,
         },
-        "cost": 3.0,
+        "cost": 180,
     }
 }
 
 resources = {
-    "water": 300,
-    "milk": 200,
+    "water": 1000,
+    "milk": 600,
     "coffee": 100,
 }
+
 
 def update_resources(water_used, milk_used, coffee_used) :
     resources["water"] -= water_used
@@ -43,36 +58,95 @@ def resources_left() :
     print(f"Water : {water_left}")
     print(f"Milk : {milk_left}")
     print(f"Coffee : {coffee_left}")
+    return (water_left + milk_left + coffee_left)
 
 
-def calculate_change(flavour, quarters_used, dimes_used, nickles_used, pennies_used) :
-    money_collected = float((quarters_used*0.25) + (dimes_used*0.10) + (nickles_used*0.05) + (pennies_used*0.01))
+def check_resources(item, flavour) :
+    #for item in MENU[flavour]["ingredients"] :
+    if resources[item] < MENU[flavour]["ingredients"][item] :
+        return False
+    return True
+
+
+def calculate_change(flavour, note100_used, note50_used, note20_used, note10_used) :
+    money_collected = float((note100_used*100) + (note50_used*50) + (note20_used*20) + (note10_used*10))
     change = money_collected - MENU[flavour]["cost"]
     return change
 
-# water_used = 0
-# milk_used = 0
-# coffee_used = 0
 
-choice = input("What would you like? (espresso/latte/cappuccino) ").lower()
+switch = "on"
+while switch == "on" :
+    
+    choice = input("What would you like? (espresso/latte/cappuccino) ").lower()
 
-if choice == "report" :
-    resources_left()
-if choice == "espresso" :
-    update_resources(water_used=MENU["espresso"]["ingredients"]["water"], coffee_used=MENU["espresso"]["ingredients"]["coffee"])
-elif choice == "latte" :
-    update_resources(water_used=MENU["latte"]["ingredients"]["water"], milk_used=MENU["latte"]["ingredients"]["milk"], coffee_used=MENU["latte"]["ingredients"]["coffee"])
-elif choice == "cappuccino" :
-    update_resources(water_used=MENU["cappuccino"]["ingredients"]["water"], milk_used=MENU["cappuccino"]["ingredients"]["milk"], coffee_used=MENU["cappuccino"]["ingredients"]["coffee"])
+    if choice == "report" :
+        resources_left()
+        continue
 
-print("Please insert coin's : ")
-quarters = float(input("How many quarters ? "))
-dimes = float(input("How many dimes ? "))
-nickles = float(input("How many nickles ? "))
-pennies = float(input("How many pennies ? "))
+    print("Please insert Note's : ")
+    note100 = float(input("How many ₹100 Notes ? "))
+    note50 = float(input("How many ₹50 Notes ? "))
+    note20 = float(input("How many ₹20 Notes ? "))
+    note10 = float(input("How many ₹10 Notes ? "))
 
-change = round(calculate_change(flavour=choice, quarters_used=quarters, dimes_used=dimes, nickles_used=nickles, pennies_used=pennies), 2)
-print(f"Here is ${change} in change. ")
+    change = round(calculate_change(flavour=choice, note100_used=note100, note50_used=note50, note20_used=note20, note10_used=note10), 2)
+    
+    if change < 0 :
+        clear()
+        print("You don't have enough money .")
+        continue
+
+
+    if choice == "espresso" :
+        if not check_resources(item="water", flavour=choice):
+            clear()
+            print(f"Sorry there is not enough water. Your Money will be refunded.")
+            continue
+        elif not check_resources(item="milk", flavour=choice):
+            clear()
+            print(f"Sorry there is not enough milk. Your Money will be refunded.")
+            continue
+        elif not check_resources(item="coffee", flavour=choice):
+            clear()
+            print(f"Sorry there is not enough coffee. Your Money will be refunded.")
+            continue
+        update_resources(water_used=MENU["espresso"]["ingredients"]["water"] ,milk_used=MENU["espresso"]["ingredients"]["milk"] , coffee_used=MENU["espresso"]["ingredients"]["coffee"])
+    elif choice == "latte" :
+        if not check_resources(item="water", flavour=choice):
+            clear()
+            print(f"Sorry there is not enough water. Your Money will be refunded.")
+            continue
+        elif not check_resources(item="milk", flavour=choice):
+            clear()
+            print(f"Sorry there is not enough milk. Your Money will be refunded.")
+            continue
+        elif not check_resources(item="coffee", flavour=choice):
+            clear()
+            print(f"Sorry there is not enough coffee. Your Money will be refunded.")
+            continue
+        update_resources(water_used=MENU["latte"]["ingredients"]["water"], milk_used=MENU["latte"]["ingredients"]["milk"], coffee_used=MENU["latte"]["ingredients"]["coffee"])
+    elif choice == "cappuccino" :
+        if not check_resources(item="water", flavour=choice):
+            clear()
+            print(f"Sorry there is not enough water. Your Money will be refunded.")
+            continue
+        elif not check_resources(item="milk", flavour=choice):
+            clear()
+            print(f"Sorry there is not enough milk. Your Money will be refunded.")
+            continue
+        elif not check_resources(item="coffee", flavour=choice):
+            clear()
+            print(f"Sorry there is not enough coffee. Your Money will be refunded.")
+            continue
+        update_resources(water_used=MENU["cappuccino"]["ingredients"]["water"], milk_used=MENU["cappuccino"]["ingredients"]["milk"], coffee_used=MENU["cappuccino"]["ingredients"]["coffee"])
+    
+
+    print(f"Here is ₹{change} in change. ")
+    print(f"Here is your {choice} ☕. Enjoy ! ")
+
+
+    switch = input("On or Off Coffee Machine : ").lower()
+    clear()
 
  
 
